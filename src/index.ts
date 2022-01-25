c5489dc5346c6f3f9373659d63070e23ec4c42c8
arbalasquide <adrian.balasquide@gmail.com>
2022-01-24 22:49:18 -0500

    Added expresiones search

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 //@ts-expect-error
 import { Client } from "irc-framework";
-import axios from "axios";
-import cheerio from "cheerio";
 import * as dotenv from "dotenv";
+import getDefinitions, { getExpressions } from "./rae";
+
 dotenv.config({ path: __dirname + "/.env" });
-
-const url = "https://dle.rae.es/";
-
-const MAX_ENTRIES = 5;
 
 const bot = new Client({
   hostname: "unaffiliated/squidbot",
@@ -34,30 +30,9 @@ bot.on("registered", () => {
 
 bot.on("message", (event: any) => {
   if (event.message.match("^\\.rae ")) {
-    const axiosInstance = axios.create();
-    const request = url + event.message.split(" ")[1];
-    axiosInstance
-      .get(encodeURI(request))
-      .then((response) => {
-        const html = response.data;
-
-        const $ = cheerio.load(html);
-        const results = $(".j").text() + $(".j2").text();
-        const regEx = new RegExp(".?[0-9]. ", "g");
-
-        let definitions: string[] = [];
-        if (results) {
-          definitions = results.split(regEx).splice(1);
-
-          for (let i = 0; i < definitions.length; i++) {
-            event.reply(definitions[i]);
-            if (i >= MAX_ENTRIES - 1) break;
-          }
-	}
-	else {
-	   event.reply("error: palabra no está en el diccionario.");
-	}
-      })
-      .catch(console.error);
+    getDefinitions(event);
+  }
+  if (event.message.match("^\\.exp ")) {
+    getExpressions(event);
   }
 });
---
+++ src/rae.ts
@@ -0,0 +1,61 @@
+import axios from "axios";
+import cheerio from "cheerio";
+
+const url = "https://dle.rae.es/";
+const MAX_ENTRIES = 5;
+
+export default async function getDefinitions(event: any) {
+    const axiosInstance = axios.create();
+    const request = url + event.message.split(" ")[1];
+    axiosInstance
+        .get(encodeURI(request))
+        .then((response) => {
+            const html = response.data;
+
+            const $ = cheerio.load(html);
+            const results = $(".j").text() + $(".j2").text();
+            const regEx = new RegExp(".?[0-9]. ", "g");
+
+            let definitions: string[] = [];
+            if (results) {
+                definitions = results.split(regEx).splice(1);
+
+                for (let i = 0; i < definitions.length; i++) {
+                    event.reply(definitions[i]);
+                    if (i >= MAX_ENTRIES - 1) break;
+                }
+            }
+            else {
+                event.reply("error: palabra no está en el diccionario.");
+            }
+        })
+        .catch(console.error);
+}
+
+export async function getExpressions(event: any) {
+    const axiosInstance = axios.create();
+    const request = url + event.message.split(" ")[1];
+    axiosInstance
+        .get(encodeURI(request))
+        .then((response) => {
+            const html = response.data;
+
+            const $ = cheerio.load(html);
+            const results = $(".j").text() + $(".j2").text();
+            const regEx = new RegExp(".?[0-9]. expr", "g");
+
+            let definitions: string[] = [];
+            if (results) {
+                definitions = results.split(regEx).splice(1);
+
+                for (let i = 0; i < definitions.length; i++) {
+                    event.reply(definitions[i]);
+                    if (i >= MAX_ENTRIES - 1) break;
+                }
+            }
+            else {
+                event.reply("error: expresion con esa palabra no se ha encontrado.");
+            }
+        })
+        .catch(console.error);
+}
